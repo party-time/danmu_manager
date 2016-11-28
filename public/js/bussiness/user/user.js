3a2f956a2335ef2273993da043647ad95fe03f5a
liuwei@party-time.cn <jushidai123>
2016-11-28 16:38:28 +0800

    用户管理

---
+++ public/js/bussiness/user/user.js
@@ -0,0 +1,87 @@
+var tableUrl = '/v1/api/admin/wechatmanager/page';
+var columnsArray = [
+    {
+        title: '序号',
+        align: 'center',
+        formatter: function (value, row, index) {
+            return index+1;
+        }
+    },
+    {
+        field: 'imgUrl',
+        title: '微信头像',
+        align: 'center',
+        width:'30%',
+        formatter: function (value, row, index) {
+            return '<img width="30%" src="'+row.imgUrl+'" />';
+        }
+    },
+    {
+        field: 'nick',
+        title: '微信昵称',
+        align: 'center'
+    },
+    {
+        field: 'sex',
+        title: '性别',
+        align: 'center',
+        formatter: function (value, row, index) {
+            if(row.sex == 1){
+                return '男';
+            }else if(row.sex == 2){
+                 return '女';
+            }else{
+                return '未知';
+            }
+
+        }
+    },
+    {
+        field: 'id', title: '操作',
+        align: 'center',
+        formatter: function (value, row, index) {
+            return '<a class="btn" onclick="delUser(\''+row.id+'\',\''+row.nick+'\')">删除</a>';
+        },
+        events: 'operateEvents'
+    }
+];
+var quaryObject = {
+    pageSize: 20
+};
+
+
+var searchNick = function(){
+    var nick = $("#searchNick").val();
+    if( '' != nick){
+        quaryObject = {
+            nick:nick,
+            pageSize:20
+        }
+        $.initTable('tableList', columnsArray, quaryObject, tableUrl);
+    }
+}
+
+var delUser = function(id,nick){
+    if (confirm('确认要删除用户“' + nick + '”吗？')) {
+
+             $.danmuAjax('/v1/api/admin/wechatmanager/del?id='+id, 'GET','json',null, function (data) {
+              if (data.result == 200) {
+                  console.log(data);
+
+                  var nick = $("#searchNick").val();
+                  if( '' != nick){
+                       searchNick();
+                  }else{
+                        $.initTable('tableList', columnsArray, quaryObject, tableUrl);
+                  }
+              }else{
+                 alert('删除失败')
+              }
+            }, function (data) {
+                console.log(data);
+            });
+        }
+}
+
+//加载表格数据
+$.initTable('tableList', columnsArray, quaryObject, tableUrl);
