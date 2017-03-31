bf6929524b01d4671b8eb44310e930355cdb2795
soaryang <759620299@qq.com>
2017-03-31 14:00:26 +0800

    bug修改。弹幕发送给后台，带上弹幕id

--- public/js/bussiness/danmu/danmu.danmucheck.js
+++ public/js/bussiness/danmu/danmu.danmucheck.js
@@ -264,7 +264,7 @@
             danmu.isSend=true;
             webSocketSendMessage({
                 type: danmu.type,
-                danmu: {message: danmu.msg, color: danmu.color, openId: danmu.openId}
+                danmu: {message: danmu.msg, id:danmu.id,color: danmu.color, openId: danmu.openId}
             });
         }
         /**
@@ -596,7 +596,7 @@
                         danmu.isSend=true;
                         webSocketSendMessage({
                             type: danmu.type,
-                            danmu: {message: danmu.msg, color: danmu.color, openId: danmu.openId}
+                            danmu: {message: danmu.msg,id:danmu.id, color: danmu.color, openId: danmu.openId}
                         });
                     }
                 }
