3899b2ed6850f7b81f7ace3cd997c4ae82912fbd
soaryang <759620299@qq.com>
2017-04-07 11:13:14 +0800

    删除代码恢复

--- public/js/bussiness/address/address.js
+++ public/js/bussiness/address/address.js
@@ -505,8 +505,6 @@ Date.prototype.format = function(f){
 }
 
 var openUpdateDialog = function(addressName,addressId){
-    $('#modalBody').empty();
-    $('#modalBody').html('<table id="screenTableList" class="table table-striped" table-height="360"></table>');
     var updatePlanTableUrl = '/v1/api/admin/updateplan/page';
     var updatePlanObject = {
         addressId:addressId,
@@ -726,17 +724,17 @@ var saveUpdatePlan = function(addressName,addressId,versionId){
 var openControlDialog = function(addressName,addressId){
     g_addressId = addressId;
     var htmlStr = '<form id="edit-profile" class="form-horizontal"><div class="control-group" style="margin-top: 18px;">';
-    htmlStr +='<label class="control-label" style="width:80px">投影相关</label><div class="controls" style="margin-left:80px;">'+
+    htmlStr +='<label class="control-label" style="width:60px">投影相关</label><div class="controls" style="margin-left:60px;">'+
                     '<a class="btn" onclick="sendControl(\'projectStart\')">投影开启</a> <a class="btn" onclick="sendControl(\'projectClose\')">投影关闭</a> <a class="btn" onclick="sendControl(\'projectChange\')">投影切白</a></div><br>'+
-                    '<label class="control-label" style="width:80px">app相关</label><div class="controls" style="margin-left:80px;">'+
+                    '<label class="control-label" style="width:60px">app相关</label><div class="controls" style="margin-left:60px;">'+
                     '<a class="btn" onclick="sendControl(\'appRestart\')">重启</a> <a class="btn" onclick="sendControl(\'appStart\')">开启</a> <a class="btn" onclick="sendControl(\'appClose\')">关闭</a></div><br>'+
-                    '<label class="control-label" style="width:80px">升级相关</label><div class="controls" style="margin-left:80px;">'+
+                    '<label class="control-label" style="width:60px">升级相关</label><div class="controls" style="margin-left:60px;">'+
                     '<a class="btn" onclick="sendControl(\'flashUpdate\')">flash升级</a> <a class="btn" onclick="sendControl(\'flashRollBack\')">flash还原</a> <a class="btn" onclick="sendControl(\'javaUpdate\')">java升级</a><a class="btn" onclick="sendControl(\'javaRollBack\')">java还原</a></div><br>'+
-                    '<label class="control-label" style="width:80px">下载相关</label><div class="controls" style="margin-left:80px;">'+
+                    '<label class="control-label" style="width:60px">下载相关</label><div class="controls" style="margin-left:60px;">'+
                     '<a class="btn" onclick="sendControl(\'videoDown\')">特效视频下载</a> <a class="btn" onclick="sendControl(\'expressionDown\')">表情下载</a> <a class="btn" onclick="sendControl(\'specialImgDown\')">特效图片下载</a><a class="btn" onclick="sendControl(\'timerDmDown\')">定时弹幕下载</a><a class="btn" onclick="sendControl(\'adDmDown\')">广告弹幕下载</a></div><br>'+
-                    '<label class="control-label" style="width:80px">配置表</label><div class="controls" style="margin-left:80px;">'+
+                    '<label class="control-label" style="width:60px">配置表</label><div class="controls" style="margin-left:60px;">'+
                     '<a class="btn" onclick="sendControl(\'configCreate\')">生成配置表</a></div><br>'+
-                    '<label class="control-label" style="width:80px">teamView</label><div class="controls" style="margin-left:80px;">'+
+                    '<label class="control-label" style="width:60px">teamView</label><div class="controls" style="margin-left:60px;">'+
                     '<a class="btn" onclick="sendControl(\'teamViewStart1\')">开启左侧</a><a class="btn" onclick="sendControl(\'screenPic1\')">左侧截图</a><a class="btn" onclick="sendControl(\'teamViewClose1\')">关闭左侧</a><a class="btn" onclick="sendControl(\'teamViewStart2\')">开启右侧</a><a class="btn" onclick="sendControl(\'screenPic2\')">右侧截图</a><a class="btn" onclick="sendControl(\'teamViewClose2\')">关闭右侧</a></div><br>';
     htmlStr+='</div></div></form>';
     $('#myModalLabel').html(addressName+'的控制台');
