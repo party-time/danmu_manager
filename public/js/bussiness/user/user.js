f8e2765d5fe293460e022a1d6b3b44ec6ece14d7
liuwei@party-time.cn <jushidai123>
2017-04-14 15:44:08 +0800

    修改用户查询逻辑

--- public/js/bussiness/user/user.js
+++ public/js/bussiness/user/user.js
@@ -1,3 +1,17 @@
+Date.prototype.format = function(f){
+    var o ={
+        "M+" : this.getMonth()+1, //month
+        "d+" : this.getDate(),    //day
+        "h+" : this.getHours(),   //hour
+        "m+" : this.getMinutes(), //minute
+        "s+" : this.getSeconds(), //second
+        "q+" : Math.floor((this.getMonth()+3)/3),  //quarter
+        "S" : this.getMilliseconds() //millisecond
+    }
+    if(/(y+)/.test(f))f=f.replace(RegExp.$1,(this.getFullYear()+"").substr(4 - RegExp.$1.length));
+    for(var k in o)
+        if(new RegExp("("+ k +")").test(f))f = f.replace(RegExp.$1,RegExp.$1.length==1 ? o[k] : ("00"+ o[k]).substr((""+ o[k]).length));return f
+}
 var tableUrl = '/v1/api/admin/wechatmanager/page';
 var columnsArray = [
     {
@@ -8,20 +22,26 @@ var columnsArray = [
         }
     },
     {
-        field: 'id',
         title: '微信编号',
         align: 'center',
+        formatter: function (value, row, index) {
+            if(row.wechatUser){
+                return row.wechatUser.id;
+            }
+        }
     },
     {
         title: '微信头像',
         align: 'center',
         width:'30%',
         formatter: function (value, row, index) {
-            return '<img width="30%" src="'+row.imgUrl+'" />';
+            if(row.wechatUser){
+                return '<img width="30%" src="'+row.wechatUser.imgUrl+'" />';
+            }
         }
     },
     {
-        field: 'nick',
+        field: 'wechatUser.nick',
         title: '微信昵称',
         align: 'center'
     },
@@ -29,9 +49,9 @@ var columnsArray = [
         title: '性别',
         align: 'center',
         formatter: function (value, row, index) {
-            if(row.sex == 1){
+            if(row.wechatUser.sex == 1){
                 return '男';
-            }else if(row.sex == 2){
+            }else if(row.wechatUser.sex == 2){
                  return '女';
             }else{
                 return '未知';
@@ -43,9 +63,9 @@ var columnsArray = [
         title: '关注状态',
         align: 'center',
         formatter: function (value, row, index) {
-            if(row.subscribeState == 0){
+            if(row.wechatUser.subscribeState == 0){
                 return '关注中';
-            }else if(row.subscribeState == 1){
+            }else if(row.wechatUser.subscribeState == 1){
                  return '取消关注';
             }else{
                 return '未知';
@@ -53,10 +73,50 @@ var columnsArray = [
         }
     },
     {
+        title: '注册场地',
+        align: 'center',
+        formatter: function (value, row, index) {
+            if(row.registAddress){
+                return row.registAddress.name;
+            }else {
+                return '未知';
+            }
+        }
+    },
+    {
+        title: '注册时间',
+        align: 'center',
+        formatter: function (value, row, index) {
+            if(row.wechatUserInfo){
+                return new Date(parseInt(row.wechatUserInfo.registDate)).format('yyyy-MM-dd hh:mm:ss');
+            }
+        }
+    },
+    {
+        title: '最后登录场地',
+        align: 'center',
+        formatter: function (value, row, index) {
+            if(row.lastAddress){
+                return row.lastAddress.name;
+            }else {
+                return '未知';
+            }
+        }
+    },
+    {
+        title: '最后登录时间',
+        align: 'center',
+        formatter: function (value, row, index) {
+            if(row.wechatUserInfo){
+                return new Date(parseInt(row.wechatUserInfo.lastOpenDate)).format('yyyy-MM-dd hh:mm:ss');
+            }
+        }
+    },
+    {
         title: '操作',
         align: 'center',
         formatter: function (value, row, index) {
-            return '<a class="btn" onclick="delUser(\''+row.id+'\',\''+row.nick+'\')">删除</a>';
+            return '<a class="btn" onclick="delUser(\''+row.wechatUser.id+'\',\''+row.wechatUser.nick+'\')">删除</a>';
         },
         events: 'operateEvents'
     }
