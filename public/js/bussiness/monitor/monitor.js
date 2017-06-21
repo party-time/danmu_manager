9575b5204bb35d28fe776b21a24b5a4a43cb8283
liuwei@party-time.cn <jushidai123>
2017-06-21 12:25:48 +0800

    新增报警管理

--- public/js/bussiness/monitor/monitor.js
+++ public/js/bussiness/monitor/monitor.js
@@ -21,8 +21,7 @@ var columnsArray = [
         title: '操作',
         align: 'center',
         formatter: function (value, row, index) {
-            return '<a class="btn" onclick="openUpdateH5temp(\''+row.id+'\')">修改</a>'+
-            '<a class="btn" onclick="del(\''+row.id+'\',\''+row.tempTitle+'\')">删除</a>';
+            return '<a class="btn" onclick="openUpdateMonitor(\''+row.id+'\')">修改</a>';
         },
         events: 'operateEvents'
     }
@@ -53,16 +52,29 @@ var openAddMonitor = function(){
     $('#myModal').modal('show');
 }
 
-var checkKey = function(){
-    var obj ={
-        key:$('#key').val()
+var openUpdateMonitor = function(id){
+    var obj={
+        id:id
     }
-    $.danmuAjax('/v1/api/admin/monitor/countByKey', 'GET','json',obj, function (data) {
+    $.danmuAjax('/v1/api/admin/monitor/get', 'GET','json',obj, function (data) {
         if(data.result == 200) {
-            if(data.data > 0){
-                alert("key有重复,请重新填写");
-                return;
-            }
+            $('#myModalLabel').html('修改报警');
+            var htmlStr = '<form id="edit-profile" class="form-horizontal"><div class="control-group" style="margin-top: 18px;">'+
+            '<label class="control-label" style="width:60px">报警名称</label><div class="controls" style="margin-left:60px;">'+
+            '<input type="text" class="span4" id="title" value="'+data.data.title+'"> </div><br>';
+            htmlStr +='<label class="control-label" style="width:60px">报警人员id</label><div class="controls" style="margin-left:60px;">'+
+              '<textarea class="span4" id="adminIds">'+data.data.adminUserIds+'</textarea><span>多个人员id请用逗号分隔</span></div><br>';
+            htmlStr +='<label class="control-label" style="width:60px">报警key</label><div class="controls" style="margin-left:60px;">'+
+            '<input type="text" class="span4" id="key" onblur="checkKey()" value="'+data.data.key+'" oldVal="'+data.data.key+'"></div><br>';
+            htmlStr +='<label class="control-label" style="width:60px">报警内容</label><div class="controls" style="margin-left:60px;">'+
+                    '<textarea class="span4" style="height:100px" id="content">'+data.data.content+'</textarea></div><br>';
+            htmlStr +='<label class="control-label" style="width:60px">报警内容占位符</label><div class="controls" style="margin-left:60px;">'+
+                           '<span class="span4">场地名称:${addressName},用户名称:${userName}</span></div><br>';
+            htmlStr+='</div></form>';
+            $('#modalBody').html(htmlStr);
+            var buttonHtml = '<button class="btn btn-primary" onclick="update(\''+id+'\')">修改</button>';
+            $('#modalFooter').html(buttonHtml);
+            $('#myModal').modal('show');
         }else{
             alert('查询失败');
         }
@@ -70,6 +82,42 @@ var checkKey = function(){
         console.log(data);
     });
 
+}
+
+var checkKey = function(){
+    var obj ={
+        key:$('#key').val()
+    }
+    var oldVal = $('#key').attr('oldVal');
+    if( oldVal ){
+        if(oldVal != $('#key').val()){
+            $.danmuAjax('/v1/api/admin/monitor/countByKey', 'GET','json',obj, function (data) {
+                if(data.result == 200) {
+                    if(data.data > 0){
+                        alert("key有重复,请重新填写");
+                        return;
+                    }
+                }else{
+                    alert('查询失败');
+                }
+            }, function (data) {
+                console.log(data);
+            });
+        }
+    }else{
+        $.danmuAjax('/v1/api/admin/monitor/countByKey', 'GET','json',obj, function (data) {
+            if(data.result == 200) {
+                if(data.data > 0){
+                    alert("key有重复,请重新填写");
+                    return;
+                }
+            }else{
+                alert('查询失败');
+            }
+        }, function (data) {
+            console.log(data);
+        });
+    }
 }
 
 
@@ -97,6 +145,48 @@ var save = function(){
         content:$('#content').val()
     }
     $.danmuAjax('/v1/api/admin/monitor/save', 'POST','json',obj, function (data) {
+        if(data.result == 200) {
+            console.log(data);
+            $('#myModal').modal('hide');
+            $.initTable('tableList', columnsArray, quaryObject, tableUrl);
+            alert('创建成功');
+        }else{
+            if(data.result_msg){
+                alert(data.result_msg);
+            }else{
+                alert('创建失败');
+            }
+        }
+    }, function (data) {
+        console.log(data);
+    });
+}
+
+var update = function(id){
+    if( '' == $('#title').val()){
+        alert("报警名称不能为空");
+        return;
+    }
+    if( '' == $('#adminIds').val()){
+        alert('报警人员id不能为空');
+        return;
+    }
+    if( '' == $('#key').val()){
+        alert('报警的key不能为空');
+        return;
+    }
+    if( '' == $('#content').val()){
+        alert('报警的内容不能为空');
+        return;
+    }
+    var obj ={
+        id:id,
+        title:$('#title').val(),
+        adminUserIds:$('#adminIds').val(),
+        key:$('#key').val(),
+        content:$('#content').val()
+    }
+    $.danmuAjax('/v1/api/admin/monitor/update', 'POST','json',obj, function (data) {
         if(data.result == 200) {
             console.log(data);
             $('#myModal').modal('hide');
