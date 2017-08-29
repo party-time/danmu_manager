e14a8f1687c11dfcfe105edbca40061b458310c8
liuwei@party-time.cn <jushidai123>
2017-08-29 16:32:28 +0800

    deploy#修改举报弹幕

--- public/js/bussiness/report/report.js
+++ public/js/bussiness/report/report.js
@@ -23,7 +23,7 @@ var columnsArray = [
         }
     },
     {
-        title: '微信头像',
+        title: '举报人头像',
         align: 'center',
         width:'30%',
         formatter: function (value, row, index) {
@@ -35,7 +35,7 @@ var columnsArray = [
         }
     },
     {
-        title: '微信昵称',
+        title: '举报人昵称',
         align: 'center',
         formatter: function (value, row, index) {
             if(null != row && null != row.wechatUser){
@@ -46,7 +46,30 @@ var columnsArray = [
         }
     },
     {
-        title: '弹幕',
+        title: '被举报人头像',
+        align: 'center',
+        width:'30%',
+        formatter: function (value, row, index) {
+            if(null != row && null != row.wechatUser){
+                return '<img width="30%" src="'+row.danmu.url+'" />';
+            }else{
+               return "";
+            }
+        }
+    },
+    {
+        title: '举报人昵称',
+        align: 'center',
+        formatter: function (value, row, index) {
+            if(null != row && null != row.danmu.nick){
+                return row.danmu.nick;
+            }else{
+               return "";
+            }
+        }
+    },
+    {
+        title: '举报弹幕',
         align: 'center',
         formatter: function (value, row, index) {
             if(null != row && null != row.danmu ){
@@ -76,11 +99,30 @@ var columnsArray = [
         formatter: function (value, row, index) {
               return new Date(parseInt(row.report.createTime)).format('yyyy-MM-dd hh:mm:ss');
         }
+    },
+    {
+        title: '操作',
+        align: 'center',
+        formatter: function (value, row, index) {
+            if( null == row.report.status ){
+                return "<a class="btn" >屏蔽弹幕</a><a class="btn" >不做处理</a>";
+            }
+        }
     }
+
 ];
 var quaryObject = {
     pageSize: 20
 };
+
+
+var blockDanmu = function(){
+
+}
+
+var noProcess = function(){}
+
+}
 
 
 //加载表格数据
