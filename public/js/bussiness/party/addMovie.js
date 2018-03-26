cb10b600cf7877cdcc5b95d316103e0420bf7eff
liuwei@party-time.cn <jushidai123>
2018-03-26 11:49:26 +0800

    deploy#新增豆瓣评分

--- public/js/bussiness/party/addMovie.js
+++ public/js/bussiness/party/addMovie.js
@@ -3,7 +3,7 @@ var findMovie = function(){
         if( data.result == 200){
             var movieList = data.data;
             for(var i=0;i<movieList.length;i++){
-                $('#selectMovie').append("<option value='"+movieList[i].id+"'>"+movieList[i].name+"</option>");
+                $('#selectMovie').append("<option value='"+movieList[i].id+"'>"+movieList[i].name+" 评分:"+movieList[i].score+"</option>");
             }
         }
     }, function (data) {
