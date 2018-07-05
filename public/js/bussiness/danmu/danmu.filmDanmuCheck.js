410abe90372e6e6850c31c307daa3612fb0366d1
soaryang <759620299@qq.com>
2018-07-05 11:30:49 +0800

    commit brach (yangtengfei)

--- public/js/bussiness/danmu/danmu.filmDanmuCheck.js
+++ public/js/bussiness/danmu/danmu.filmDanmuCheck.js
@@ -1,3 +1,17 @@
+Date.prototype.format = function(f){
+    var o ={
+        "M+" : this.getMonth()+1, //month
+        "d+" : this.getDate(),    //day
+        "h+" : this.getHours(),   //hour
+        "m+" : this.getMinutes(), //minute
+        "s+" : this.getSeconds(), //second
+        "q+" : Math.floor((this.getMonth()+3)/3),  //quarter
+        "S" : this.getMilliseconds() //millisecond
+    }
+    if(/(y+)/.test(f))f=f.replace(RegExp.$1,(this.getFullYear()+"").substr(4 - RegExp.$1.length));
+    for(var k in o)
+        if(new RegExp("("+ k +")").test(f))f = f.replace(RegExp.$1,RegExp.$1.length==1 ? o[k] : ("00"+ o[k]).substr((""+ o[k]).length));return f
+}
 (function () {
     var quaryObject = {};
     var app = angular.module('danmuCheckApp', []);
