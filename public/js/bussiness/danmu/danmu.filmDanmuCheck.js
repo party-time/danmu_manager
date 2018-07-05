46ad2f64f96fa4697df63039c659d12f2a5f7539
soaryang <759620299@qq.com>
2018-07-05 11:32:09 +0800

    commit brach (yangtengfei)

--- public/js/bussiness/danmu/danmu.filmDanmuCheck.js
+++ public/js/bussiness/danmu/danmu.filmDanmuCheck.js
@@ -1,17 +1,4 @@
-Date.prototype.format = function(f){
-    var o ={
-        "M+" : this.getMonth()+1, //month
-        "d+" : this.getDate(),    //day
-        "h+" : this.getHours(),   //hour
-        "m+" : this.getMinutes(), //minute
-        "s+" : this.getSeconds(), //second
-        "q+" : Math.floor((this.getMonth()+3)/3),  //quarter
-        "S" : this.getMilliseconds() //millisecond
-    }
-    if(/(y+)/.test(f))f=f.replace(RegExp.$1,(this.getFullYear()+"").substr(4 - RegExp.$1.length));
-    for(var k in o)
-        if(new RegExp("("+ k +")").test(f))f = f.replace(RegExp.$1,RegExp.$1.length==1 ? o[k] : ("00"+ o[k]).substr((""+ o[k]).length));return f
-}
+
 (function () {
     var quaryObject = {};
     var app = angular.module('danmuCheckApp', []);
@@ -390,6 +377,21 @@ Date.prototype.format = function(f){
             }, function (data) {
                 console.log(data);
             });
+
+            Date.prototype.format = function(f){
+                var o ={
+                    "M+" : this.getMonth()+1, //month
+                    "d+" : this.getDate(),    //day
+                    "h+" : this.getHours(),   //hour
+                    "m+" : this.getMinutes(), //minute
+                    "s+" : this.getSeconds(), //second
+                    "q+" : Math.floor((this.getMonth()+3)/3),  //quarter
+                    "S" : this.getMilliseconds() //millisecond
+                }
+                if(/(y+)/.test(f))f=f.replace(RegExp.$1,(this.getFullYear()+"").substr(4 - RegExp.$1.length));
+                for(var k in o)
+                    if(new RegExp("("+ k +")").test(f))f = f.replace(RegExp.$1,RegExp.$1.length==1 ? o[k] : ("00"+ o[k]).substr((""+ o[k]).length));return f
+            }
 
             var tableUrl = '/v1/api/admin/historyCheckDanmu/page';
             var columnsArray = [
