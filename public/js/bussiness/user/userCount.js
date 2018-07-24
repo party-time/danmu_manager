5cd3a93ecfe7551a7d3d30af617d046eb63570bf
soaryang <759620299@qq.com>
2018-07-24 17:17:59 +0800

    commit

--- public/js/bussiness/user/userCount.js
+++ public/js/bussiness/user/userCount.js
@@ -47,123 +47,23 @@ var quaryObject = {
 };
 
 
-var searchNick = function(){
-    var nick = $("#searchNick").val();
-    if( '' != nick){
-        quaryObject = {
-            nick:nick,
-            pageSize:20
-        }
-        $.initTable('tableList', columnsArray, quaryObject, tableUrl);
-    }
-}
-
-var delUser = function(id,nick){
-    if (confirm('确认要删除用户“' + nick + '”吗？')) {
-
-        $.danmuAjax('/v1/api/admin/wechatmanager/del?id='+id, 'GET','json',null, function (data) {
-            if (data.result == 200) {
-                console.log(data);
-
-                var nick = $("#searchNick").val();
-                if( '' != nick){
-                    searchNick();
-                }else{
-                    $.initTable('tableList', columnsArray, quaryObject, tableUrl);
-                }
-            }else{
-                alert('删除失败')
-            }
-        }, function (data) {
-            console.log(data);
-        });
-    }
-}
 
 
-var openAddress = function(wechatUserId){
-    var aList = $('#selectAddress').children('a');
-    var addressIds = '';
-    if( aList && aList.length > 0){
-        for(var i=0;i<aList.length;i++){
-            addressIds += $(aList[i]).attr('addressId');
-            if( i < aList.length-1){
-                addressIds += ',';
-            }
-        }
-    }
-    var addressTableUrl = '/v1/api/admin/address/queryAll';
-    var addressQueryObject = {
-        addressIds:addressIds,
-        pageSize:6
-    }
-    var addressColumnsArray =[
-        {
-            field: 'name',
-            title: '名称',
-            align: 'center'
-        },
-        {
-            title: '操作',
-            align: 'center',
-            formatter: function (value, row, index) {
-                return '<a class="btn" onclick="assignAddress(\''+row.id+'\',\''+wechatUserId+'\')">选择</a>';
-            }
-        }
-    ];
 
-    var tableSuccess = function(){
-        $('#modalBody').find('.pull-left').remove();
-    }
-    $.initTable('addressTableList', addressColumnsArray, addressQueryObject, addressTableUrl,tableSuccess);
-    $('#myModalLabel').html('场地管理');
-    var buttonHtml = '<button class="btn" data-dismiss="modal" aria-hidden="true">关闭</button>';
-    $('#modalFooter').html(buttonHtml);
-    $('#modalody').find('.pull-left').remove();
-    $('#myModal').modal('show');
-}
-
-var sendBiaoBai = function(wechatId){
-    $.danmuAjax('/v1/api/admin/wechatmanager/sendBiaobai?wechatId='+wechatId, 'GET','json',null, function (data) {
+var getDate = function(){
+    $.danmuAjax('/v1/api/admin/wechatCount/currentYearCountDate', 'GET','json',null, function (data) {
         if(data.result == 200){
             console.log(data);
-            alert('发送成功');
         }else{
-            alert('发送失败');
+
         }
     }, function (data) {
         console.log(data);
     });
 }
+var init = function () {
+    getDate();
+    //加载表格数据
+    $.initTable('tableList', columnsArray, quaryObject, tableUrl);
 
-var dasang = function(wechatId){
-    $.danmuAjax('/v1/api/admin/wechatmanager/dashang?wechatId='+wechatId, 'GET','json',null, function (data) {
-        if(data.result == 200){
-            console.log(data);
-            alert('发送成功');
-        }else{
-            alert('发送失败');
-        }
-    }, function (data) {
-        console.log(data);
-    });
 }
-
-var assignAddress = function(addressId,wechatId){
-    $.danmuAjax('/v1/api/admin/wechatmanager/assignAddress?addressId='+addressId+'&wechatId='+wechatId, 'GET','json',null, function (data) {
-        if(data.result == 200){
-            console.log(data);
-            $('#myModal').modal('hide');
-            $.initTable('tableList', columnsArray, quaryObject, tableUrl);
-            alert('设置成功');
-
-        }else{
-            alert('设置失败');
-        }
-    }, function (data) {
-        console.log(data);
-    });
-}
-
-//加载表格数据
-$.initTable('tableList', columnsArray, quaryObject, tableUrl);
